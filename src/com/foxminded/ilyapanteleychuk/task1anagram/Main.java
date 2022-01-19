97b2db33b6d15e4dcb265c22f04b2f9d86a46605
Ilya Panteleychuk <panteleychukilia@gmail.com>
2022-01-19 02:16:25 +0200

    add checking for symbol

--- src/com/foxminded/ilyapanteleychuk/task1anagram/Main.java
+++ src/com/foxminded/ilyapanteleychuk/task1anagram/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Anagram anagram = new Anagram();
-        String outPut = anagram.reverse("ab2cd3ef4jh");
+        String outPut = anagram.reverse("ab2cd3e%f4jh");
         System.out.println(outPut);
     }       
 }
