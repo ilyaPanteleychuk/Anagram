3684268c64759653cf0914fa93aa95fe61541b67
Ilya Panteleychuk <panteleychukilia@gmail.com>
2022-01-21 12:39:28 +0200

    Added BeforeEach annotation

--- test/com/foxminded/ilyapanteleychuk/task1anagram/AnagramTest.java
+++ test/com/foxminded/ilyapanteleychuk/task1anagram/AnagramTest.java
@@ -2,56 +2,54 @@ package com.foxminded.ilyapanteleychuk.task1anagram;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AnagramTest {
 
+    Anagram reversed;
+    @BeforeEach
+    public void instanceInitialize() {
+        reversed = new Anagram();
+    }
 
     @Test
     void givenString_whenReverse_thenReturnReversedString() {
-        Anagram reversed = new Anagram();
         assertEquals("edcba", reversed.reverse("abcde"));
     }
-    
+
     @Test
     void givenSingleLetter_whenReverse_thenReturnLetter() {
-        Anagram reversed = new Anagram();
         assertEquals("a", reversed.reverse("a"));
     }
-    
+
     @Test
     void givenStringWithDiffCases_whenReverse_thenReturnReversedString() {
-        Anagram reversed = new Anagram();
         assertEquals("ASJfjkkSKJHSFA", reversed.reverse("AFSHJKSkkjfJSA"));
     }
-    
+
     @Test
     void givenStringSameLetter_whenReverse_thenReturnReversedString() {
-        Anagram reversed = new Anagram();
         assertEquals("ffffff", reversed.reverse("ffffff"));
     }
 
     @Test
     void givenNonLetterSymbols_whenReverse_thenReturnNotReversedNonLetterSymbols() {
-        Anagram reversed = new Anagram();
         assertEquals("#dc#b@a#", reversed.reverse("#ab#c@d#"));
     }
-    
+
     @Test
     void givenOneSpace_whenReverse_thenReturnReversedWithSpace() {
-        Anagram reversed = new Anagram();
         assertEquals("fe#d cb#a", reversed.reverse("ab#c de#f"));
     }
-    
+
     @Test
     void givenSeveralSpace_whenReverse_thenReturnReversedWithSpaces() {
-        Anagram reversed = new Anagram();
         assertEquals("ji#h gf#e dc@ba", reversed.reverse("ab#c de#f gh@ij"));
     }
-        
+
     @Test
     void givenEmptyString_whenReverse_thenReturnNullPointerException() {
-        Anagram reversed = new Anagram();
         assertThrows(NullPointerException.class, () -> {
             reversed.reverse("");
         });
@@ -59,7 +57,6 @@ class AnagramTest {
 
     @Test
     void givenWhiteSpaces_whenReverse_thenReturnNullPointerException() {
-        Anagram reversed = new Anagram();
         assertThrows(NullPointerException.class, () -> {
             reversed.reverse("  ");
         });
@@ -67,7 +64,6 @@ class AnagramTest {
 
     @Test
     void givenNull_whenReverse_thenReturnNullPointerException() {
-        Anagram reversed = new Anagram();
         assertThrows(NullPointerException.class, () -> {
             reversed.reverse(null);
         });
@@ -75,16 +71,11 @@ class AnagramTest {
 
     @Test
     void givenStringOfNumbers_whenReverse_thenReturnSameString() {
-        Anagram reversed = new Anagram();
         assertEquals("12345", reversed.reverse("12345"));
     }
 
     @Test
     void givenStringOfSymbols_whenReverse_thenReturnSameString() {
-        Anagram reversed = new Anagram();
         assertEquals("#%#%#!$#", reversed.reverse("#%#%#!$#"));
     }
-
 }
-
-
